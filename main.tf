f91d9ecbd19f7bcecc35359494ab257ee810670e
joseaugustolima <66323877+JoseAugustoLima@users.noreply.github.com>
2021-10-12 02:38:34 -0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+
+  backend "remote" {
+    organization = "joseaugustolima"
+    workspaces {
+      name = "Tech-Exercise"
+    }
+  }
+
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "tech_server" {
+  ami           = "ami-02e136e904f3da870"
+  instance_type = "t2.micro"
+  tags = {
+    Name = var.instance_name
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "instance_name" {
+  description = "Name tag of instance"
+  type = string
+  default = "TechExercise-EC2"
+}
