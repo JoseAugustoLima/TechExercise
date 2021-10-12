3c0df2e031c26f594adb5f6a6d36ded169bd73bc
joseaugustolima <66323877+JoseAugustoLima@users.noreply.github.com>
2021-10-12 15:59:23 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ provider "aws" {
 
 resource "aws_instance" "tech_server" {
   ami           = "ami-02e136e904f3da870"
-  instance_type = "t2.large"
+  instance_type = "t2.xlarge"
   tags = {
     Name = var.instance_name
   }
